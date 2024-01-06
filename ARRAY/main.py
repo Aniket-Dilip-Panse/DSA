69e65e819e6045f21a1441fb045aac4b916297ab
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-01-07 01:13:09 +0530

    created array

--- ARRAY/main.py
+++ ARRAY/main.py
@@ -1,3 +1,19 @@
 '''array tutorial starting from 7/1/2024 welcome to DSA with python''' 
-def main():
-    pass
+class ArrayProblem:
+    def __init__(self):
+        self.arr = []
+        self.size = 0
+
+    def insert(self, value):
+        self.arr.append(value)
+        self.size += 1
+
+    def display(self):
+        print(self.arr)
+
+    def delete(self, index):
+        del self.arr[index]
+        self.size -= 1
+
+    def update(self, index, value):
+        self.arr[index] = value
