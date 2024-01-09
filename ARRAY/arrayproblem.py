06282d89b0769441fd2b04e5de9101dad6df92df
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-01-09 23:42:06 +0530

    new array python added

--- ARRAY/arrayproblem.py
+++ ARRAY/arrayproblem.py
@@ -2,3 +2,17 @@
 import array
 arr = array.array('i', [10, 20, 30, 40, 50])
 print(arr[::-1])
+
+# maximum and minimun element in the array
+import array
+arr = array.array('i', [100, 20, 30, 40, 50])
+print(min(arr))
+print(max(arr))
+max = arr[0]
+for value in arr:
+    if value > max:
+        max = value
+        value,max = max,value
+print(max)
+print(value)
+print(arr)
